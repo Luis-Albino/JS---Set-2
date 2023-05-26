2717b2157b0203accdc50bd5d33416430b0f338c
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-05-25 22:56:13 -0600

    Update exer - 3 - a.js

--- exer - 3 - a.js
+++ exer - 3 - a.js
@@ -1,19 +1,22 @@
 function shiftCiclically (str) {
-    if (typeof str != "string") {
-        console.log("Invalid iput. String required");
+    try {
+        if (typeof str != "string") throw "Invalid iput. String required"
+    }
+    catch (err) {
         return
-    };
-
-    let lower = "abcdefghijklmnopqrstuvwxyzaABCDEFGHIJKLMNOPQRSTUVWXYZA01234567890";
-
-    for (let i=0; i<str.length; i++) {
-        let char = str.slice(i,i+1);
-        let index = lower.indexOf(char) + 1;
-        let toChar = (index > 0)?lower.slice(index,index+1):char; // To ensure it will only change numbers and digits
-        str = str.slice(0,i) + toChar + str.slice(i+1);
     }
 
+    let options = "abcdefghijklmnopqrstuvwxyzaABCDEFGHIJKLMNOPQRSTUVWXYZA01234567890";
+
+    for (let i=0; i < str.length; i++) {
+        let char = str.charAt(i);
+        if (char.match(/[a-zA-Z0-9]/)){
+            let index = options.indexOf(char);
+            let newChar = options[index + 1];
+            str = str.slice(0,i) + newChar + str.slice(i+1);
+        }
+    }
     console.log(str);
-};
+}
 
 shiftCiclically("Birthday: 08/06/1988");
