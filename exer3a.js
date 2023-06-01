e8a2a9deb65b5c9128268d27d1822cf8ef1a6faf
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-06-01 12:11:33 -0600

    Update exer3a.js

--- exer3a.js
+++ exer3a.js
@@ -1,21 +1,13 @@
 function shiftCiclically (str) {
-    try {
-        if (typeof str != "string") throw "Invalid iput. String required"
-    }
-    catch (err) {
-        return
-    }
+    if (typeof str != "string") return undefined;
 
     let options = "abcdefghijklmnopqrstuvwxyzaABCDEFGHIJKLMNOPQRSTUVWXYZA01234567890";
 
-    for (let i=0; i < str.length; i++) {
-        let char = str.charAt(i);
-        if (char.match(/[a-zA-Z0-9]/)){
-            let index = options.indexOf(char);
-            let newChar = options[index + 1];
-            str = str.slice(0,i) + newChar + str.slice(i+1);
-        }
-    }
+    str = str.replace(/[a-zA-Z0-9]/g,function (char){
+        let index = options.indexOf(char);
+        return options[index + 1];
+    });
+
     console.log(str);
 }
 
