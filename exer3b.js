a1e15f6fb56ba537ce25b2b4250520de4bceeee3
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-06-01 16:37:26 -0600

    Update exer3b.js

--- exer3b.js
+++ exer3b.js
@@ -1,15 +1,27 @@
 function hashtag (word,nodeElement) {
-    if ((typeof word != "string") || !nodeElement) {
-        console.log("Invalid iput");
-        return
-    };
+    word = word.match(/\b\w+\b/)
 
     let text = nodeElement.innerHTML;
-    text = text;
     text = text.replaceAll(word,"<a href=\"https://twitter.com/search?q=%23"+word+"\">#"+word+"</a>");
     nodeElement.innerHTML = text;
 }
 
 let node = document.getElementById("text");
 
-hashtag("rocket",node);
+let button = document.getElementById("btn");
+button.addEventListener("click",function () {
+    let word = tag.value;
+    hashtag (word,node);
+    button.disabled = true;
+    tag.value = ""
+})
+
+let tag = document.getElementById("hashtag")
+tag.addEventListener("input",function () {
+    if (!tag.value) {
+        button.disabled = true;
+    }
+    else {
+        button.disabled = false;
+    };
+})
