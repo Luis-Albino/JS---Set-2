eb9cea2d496040ab50f368cc509bcf8e5f96f9db
Luis-Albino <122559528+Luis-Albino@users.noreply.github.com>
2023-08-10 17:26:29 -0600

    Update exer3b.js

--- exer3b.js
+++ exer3b.js
@@ -1,12 +1,14 @@
 function hashtag (word,nodeElement) {
     word = word.match(/\b\w+\b/)
 
-    let text = nodeElement.innerText;;
-    text = text.replaceAll(word,"<a href=\"https://twitter.com/search?q=%23"+word+"\">#"+word+"</a>");
+    // let text = nodeElement.innerText;
+    let text = nodeElement.originalText;
+    text = text.replaceAll(word[0],'<a href=\"https://twitter.com/search?q=%23'+word[0]+'\">#'+word[0]+'</a>');
     nodeElement.innerHTML = text;
 }
 
 let node = document.getElementById("text");
+node.originalText = node.innerText;
 
 let button = document.getElementById("btn");
 button.addEventListener("click",function () {
